2c431616a3fdefc86150b9728c3deb30c8865602
Pierre Ducher <pierre.ducher@365talents.com>
2021-05-11 22:43:40 +0200

    schedule the call to check every two minutes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const https = require("https")
 const sgMail = require('@sendgrid/mail');
+const cron = require('node-cron');
 
 const APPOINTMENT_API = 'www.doctolib.de';
 const API_PATH = "/availabilities.json";
@@ -19,50 +20,54 @@ function getConf() {
 const conf = getConf();
 sgMail.setApiKey(conf.sendgridApiKey);
 
-https
-  .request(
-    {
-      hostname: APPOINTMENT_API,
-      path: `${API_PATH}?start_date=2021-05-11&visit_motive_ids=2495719&agenda_ids=397800-397776-402408-397766&insurance_sector=public&practice_ids=158431&destroy_temporary=true&limit=4`,
-    },
-    res => {
-      let data = "";
+function queryAPIForAppointment() {
+    https
+    .request(
+      {
+        hostname: APPOINTMENT_API,
+        path: `${API_PATH}?start_date=2021-05-11&visit_motive_ids=2495719&agenda_ids=397800-397776-402408-397766&insurance_sector=public&practice_ids=158431&destroy_temporary=true&limit=4`,
+      },
+      res => {
+        let data = "";
+  
+        res.on("data", d => {
+          data += d;
+        })
+        res.on("end", () => {
+          /**
+           * data: {"availabilities":[],"total":0,"reason":"no_availabilities","message":"Diese Termine stehen zu einem späteren Zeitpunkt wieder für eine Online-Buchung zur Verfügung. ","number_future_vaccinations":79818}
+           */
+          const parsedData = JSON.parse(data);
+          if(parsedData.availabilities) {
+              const nbAvail = parsedData.availabilities.length;
+              const nextSlot = parsedData.next_slot;
+              console.log(`Number of availabilities: ${nbAvail}`);
+              console.log(`Next slot: ${nextSlot}`);
+  
+              if(nbAvail > 0) {
+                  const emailContent = `There are at least ${parsedData.availabilities.length} appointments available. \n Next slot date: ${nextSlot}`;
+                  sgMail.send({
+                      from: conf.email.from,
+                      to: conf.email.to,
+                      subject: conf.email.subject,
+                      html: emailContent,
+                      text: emailContent,
+                    })
+                    .then((response) => {
+                      console.log(response[0].statusCode)
+                      console.log(response[0].headers)
+                    })
+                    .catch((error) => {
+                      console.error(error)
+                    });
+              } else {
+                  console.log('No slot available :( sorry Brubo');
+              }
+          }
+        })
+      }
+    )
+    .end();
+}
 
-      res.on("data", d => {
-        data += d;
-      })
-      res.on("end", () => {
-        /**
-         * data: {"availabilities":[],"total":0,"reason":"no_availabilities","message":"Diese Termine stehen zu einem späteren Zeitpunkt wieder für eine Online-Buchung zur Verfügung. ","number_future_vaccinations":79818}
-         */
-        const parsedData = JSON.parse(data);
-        if(parsedData.availabilities) {
-            const nbAvail = parsedData.availabilities.length;
-            const nextSlot = parsedData.next_slot;
-            console.log(`Number of availabilities: ${nbAvail}`);
-            console.log(`Next slot: ${nextSlot}`);
-
-            if(nbAvail > 0) {
-                const emailContent = `There are at least ${parsedData.availabilities.length} appointments available. \n Next slot date: ${nextSlot}`;
-                sgMail.send({
-                    from: conf.email.from,
-                    to: conf.email.to,
-                    subject: conf.email.subject,
-                    html: emailContent,
-                    text: emailContent,
-                  })
-                  .then((response) => {
-                    console.log(response[0].statusCode)
-                    console.log(response[0].headers)
-                  })
-                  .catch((error) => {
-                    console.error(error)
-                  });
-            } else {
-                console.log('No slot available :( sorry Brubo');
-            }
-        }
-      })
-    }
-  )
-  .end();
+  cron.schedule('*/2 * * * *', queryAPIForAppointment);
